ab4aa094a2bef295c771e45dbe8e4da0aa525add
dev <Dev>
2023-05-02 21:34:04 +0530

    changes in test cases

--- backend/src/model/user.model.js
+++ backend/src/model/user.model.js
@@ -5,7 +5,6 @@ const userSchema = mongoose.Schema({
     lastName: { type: String, allowNull: false },
     positionId: {  type: mongoose.SchemaTypes.ObjectId,
         ref: 'Position' },
-    role: { type: Number, allowNull: false, defaultValue: 0, comment: '1=Director , 2=SeniorManager, 3=JuniorManager, 4=SeniorDeveloper , 5=JuniorDeveloper' },
     employeeNumber: { type: String, allowNull: false },
 })
 
